8bbf18257ceacd0b07064c124c2f6434aa24ec70
Bharat <yadavbharat163@gmail.com>
2019-09-18 15:24:16 +0530

    Updates

---
+++ precache-manifest.385e34f53e6a56c7ded4e49d8c4c50d4.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "846785fdf52feb7d54da2dce74674e94",
+    "url": "/karmaapp/index.html"
+  },
+  {
+    "revision": "8e088c45a0dd2e5a4886",
+    "url": "/karmaapp/static/css/2.92583b84.chunk.css"
+  },
+  {
+    "revision": "86c7ab44105f44ac3955",
+    "url": "/karmaapp/static/css/main.65013fef.chunk.css"
+  },
+  {
+    "revision": "8e088c45a0dd2e5a4886",
+    "url": "/karmaapp/static/js/2.4d17177a.chunk.js"
+  },
+  {
+    "revision": "86c7ab44105f44ac3955",
+    "url": "/karmaapp/static/js/main.da2fabf0.chunk.js"
+  },
+  {
+    "revision": "cb6e515abc0e55e0a14a",
+    "url": "/karmaapp/static/js/runtime~main.a42cb50a.js"
+  },
+  {
+    "revision": "4e3e0413220abe4ce735617cdce62ce2",
+    "url": "/karmaapp/static/media/3.4e3e0413.jpg"
+  },
+  {
+    "revision": "bbc8994b84001f4c97ebc81d7e3a7a7b",
+    "url": "/karmaapp/static/media/4.bbc8994b.jpg"
+  },
+  {
+    "revision": "ea01997f0b3c5e25b42fd85f838a0a69",
+    "url": "/karmaapp/static/media/5.ea01997f.jpg"
+  },
+  {
+    "revision": "f5b2b07eb27de79eea159ba9bd0ffe7e",
+    "url": "/karmaapp/static/media/Adot.f5b2b07e.png"
+  },
+  {
+    "revision": "4d418822a1fd8a7d2e99423f99841e79",
+    "url": "/karmaapp/static/media/bg-02-1920x660.4d418822.jpg"
+  },
+  {
+    "revision": "bbc8994b84001f4c97ebc81d7e3a7a7b",
+    "url": "/karmaapp/static/media/bg-banner1.bbc8994b.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/karmaapp/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/karmaapp/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/karmaapp/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/karmaapp/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/karmaapp/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "d0ccff4dff3e52ed50560039d31bc5a7",
+    "url": "/karmaapp/static/media/postback.d0ccff4d.png"
+  },
+  {
+    "revision": "b28fd2eaff4c4733fe6ee88f3ec1e49e",
+    "url": "/karmaapp/static/media/testi-bg.b28fd2ea.jpg"
+  }
+]);
